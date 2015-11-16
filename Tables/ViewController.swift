89b445d19b0b3be3cc58544324023fe2adf04613
Estelle <estelleccl@gmail.com>
2015-11-16 23:25:39 +0800

    will still listening to 5 rows - eventhou the textlabel only upto row 3

--- Tables/ViewController.swift
+++ Tables/ViewController.swift
@@ -32,15 +32,18 @@ extension ViewController : UITableViewDataSource {
     
     //numberOfRowsInSection
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //will have 3 rows
-        return 3
+        //will still listening to 5 rows - eventhou the textlabel only upto row 3
+        return 5
     }
     
     //cellForRowAtIndexPath - what is the cell that I should display for the row at the index path(table section at row grouping pairs - sec 0 row 1/sec 0 row 2)
     //able to add footer for section
+    
+    //what is the cell for the row
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
         
+        //custom cell
         if indexPath.row == 0 {
             cell.textLabel?.text = "Row Zero"
         } else if indexPath.row == 1 {
@@ -49,11 +52,14 @@ extension ViewController : UITableViewDataSource {
             cell.textLabel?.text = "Row Two"
         }
         
+        cell.textLabel?.text = ""
         return cell
     }
 }
 
+//respond on click on the cell
 extension ViewController: UITableViewDelegate {
+
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         print("User selected row \(indexPath.row)")
     }
